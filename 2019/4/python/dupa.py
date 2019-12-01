b1cef5faaca9d1de75e6a564f131a56cb1202fd9
telecorpse <saletrapotasowatosyf@gmail.com>
2019-12-01 01:02:47 +0100

    finito pablito

--- 2019/4/python/dupa.py
+++ 2019/4/python/dupa.py
@@ -1,7 +1,7 @@
 import math as M
 
 
-f = open("przyklad.txt")
+f = open("liczby.txt")
 liczby = f.read().split()
 f.close()
 
@@ -27,13 +27,30 @@ for l in liczby:
 		s_silni += M.factorial(int(cyfra))
 	if x == s_silni:
 		odp += l + "\n"
+odp += "\n"
 
-
+odp += "4.3:\n"
+elem_best = liczby[0]
+dlug_best = 1
+nwd_best = 1
 for i in range(0, len(liczby) - 1):
 	x1 = int(liczby[i])
 	x2 = int(liczby[i + 1])
-	print(M.gcd(x1, x2))
-#tutaj póki co wymiękam
+	nwd = M.gcd(x1, x2)
+	if nwd > 1:
+		elem = x1
+		dlug = 2
+		for j in range(i + 2, len(liczby)):
+			nwd2 = M.gcd(nwd, int(liczby[j]))
+			if nwd2 == 1:
+				break
+			nwd = nwd2
+			dlug += 1
+		if dlug > dlug_best:
+			dlug_best = dlug
+			elem_best = elem
+			nwd_best = nwd
+odp += str(elem_best) + " " + str(dlug_best) + " " + str(nwd_best) + "\n"
 
 f = open("wyniki4.txt", "w")
 f.write(odp)
