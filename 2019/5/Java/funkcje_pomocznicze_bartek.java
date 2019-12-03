cd81b625f42431e437ed6c3d0fb95592a46e0bd6
czorekk <48157349+czorekk@users.noreply.github.com>
2019-12-03 19:28:13 +0100

    Create funkcje_pomocznicze_bartek.java

---
+++ 2019/5/Java/funkcje_pomocznicze_bartek.java
@@ -0,0 +1,16 @@
+public static int silnia(int a) {
+		int b = 1;
+		for (int i = 1; i <= a; i++) {
+			b = b * i;
+		}
+		return b;
+	}
+	
+	public static int nwd(int a, int b) {
+		for (int i = a; i >= 1; i--) {
+			if(a % i == 0 && b % i == 0) {
+				return i;
+			}
+		}
+		return 0;
+	}
